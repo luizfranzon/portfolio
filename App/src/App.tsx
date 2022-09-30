bd48d608f19ef15924198fa7ab0f7dc96929dc54
Luiz <luizfranzon05@hotmail.com>
2022-09-30 15:29:15 -0300

    ✨ Set React routes

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Pages---------------
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Knowledge } from "./pages/Knowledge";
+import { Contact } from "./pages/Contact";
+//--------------------
+
 export function App() {
     return (
-        <div>
-            <h1 className="">Hello World</h1>
-        </div>
-    );
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/sobre" element={<About/>}/>
+                <Route path="/conhecimentos" element={<Knowledge/>}/>
+                <Route path="/contato" element={<Contact/>}/>
+                <Route path="*" element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
