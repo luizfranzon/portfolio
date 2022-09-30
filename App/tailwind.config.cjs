befa246b47297a00708683b474841b5b4f024eba
Luiz <luizfranzon05@hotmail.com>
2022-09-30 14:46:00 -0300

    💄Added custom fonts to tailwindcss config file
    Sora and Kanit

--- App/tailwind.config.cjs
+++ App/tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
+        fontFamily: {
+            'kanit' : ["Kanit", "system-ui"],
+            'sora' : ["Sora", "sans-serif"]
+        }
     },
     plugins: [],
 };
