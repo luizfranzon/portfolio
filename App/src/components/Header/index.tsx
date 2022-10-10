e955a58ad89b9dd5c5b9bb60b75765bceffd2886
Luiz <luizfranzon05@hotmail.com>
2022-10-10 18:00:29 -0300

    🔥Cleanup

--- App/src/components/Header/index.tsx
+++
@@ -1,9 +0,0 @@
-import logoMobile from "../../assets/mobile/logoMobile.svg";
-
-export function Header() {
-    return (
-        <header className="bg-[#080C14] flex items-center justify-center py-5">
-            <img src={logoMobile} alt="Logo do site escrito luizfranzon.dev"/>
-        </header>
-    );
-}
