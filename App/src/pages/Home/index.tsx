e1f31c15e96f5563a00a075e81afdb14b0583895
Luiz <luizfranzon05@hotmail.com>
2022-11-12 02:28:32 -0300

    🚧 Work in progress

--- App/src/pages/Home/index.tsx
+++ App/src/pages/Home/index.tsx
@@ -1,11 +1,15 @@
 import { Presentation } from "./components/Presentation";
 import { Projects } from "./components/Projects";
+
 import "./Home.scss";
+
+import blueCircle from "../../assets/blurCicle.svg";
 
 export function Home() {
     return (
         <>
             <section className="homeSection">
+                <img className="blueCircle noselect" alt="" src={blueCircle} />
                 <Presentation />
             </section>
             <div className="blueLine"></div>
